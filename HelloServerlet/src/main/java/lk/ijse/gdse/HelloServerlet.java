ecde1e942bed9facf52c309cbf8a2731cbe7f4da
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-09-21 19:59:20 +0530

    generate little response

--- HelloServerlet/src/main/java/lk/ijse/gdse/HelloServerlet.java
+++ HelloServerlet/src/main/java/lk/ijse/gdse/HelloServerlet.java
@@ -1,2 +1,19 @@
-package lk.ijse.gdse;public class HelloServerlet {
+package lk.ijse.gdse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloServerlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Request ekak labuna !");
+        PrintWriter writer = resp.getWriter();
+        writer.write("Response send !");
+    }
 }
